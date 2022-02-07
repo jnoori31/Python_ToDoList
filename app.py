74ebeb0507ade2197736acc5da8bd1fe9a561ca8
Jonas Noori jonasgeneralassembly@gmail.com <jonasgeneralassembly@gmail.com>
2022-02-07 20:05:09 +0000

    databse setup failed

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 
 app = Flask(__name__)
 
+app.config['SQLACHEMY_DATABASE_URI'] = 'mysql://flaskuser:flask@localhost/flask'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/', methods = ['Get'])
+@app.route('/get', methods = ['Get'])
 def get_articles():
 	return jsonify({"Hello":"Jonas"})
 
 
+db = SQLAlchemy(app)
 
+class Articles(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(100))
+	title = db.Column(db.Text())
+	date = db.Column(db.DateTime, default = datetime.datetime.now)
+
+	def __init__(self, title, body):
+		self.title = title 
+		self.body = body
 
 if __name__ == "__main__":
 	app.run(debug=True, host='0.0.0.0', port=5000)
